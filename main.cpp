2f51da5c3a0d4cfb93acff3101c25a533fd50ac9
cjmac2099 <cjmac2017@gmail.com>
2025-03-01 15:53:36 -0800

    working system

--- main.cpp
+++ main.cpp
@@ -9,21 +9,24 @@ using namespace std;
 // Functions 
 bool didYouWorkout(string prompt);
 
+static int streak = 0;
 
 int main(int argc, char* argv[]){
 
     string prompt = "Hey man! You know the dream; get ripped and make software. A true tech bro's dream.\n"
-                    "So for starters, have you worked out today?";
+                    "So for starters, have you worked out today?(y/n): ";
 
     bool status = didYouWorkout(prompt);
 
     if(status){
-        
-
+        cout << "\nYESUHHHH\nKEEP IT UP MAN! COME BACK TOMORROW TO KEEP YOUR STREAK." << endl; 
+        streak++;
     } else {
-
+        cout << "\nbruh. Come on, how are we suppose to be eat wings and sip martinis at this rate. LOCK IN!" << endl;
+        streak = 0;
     }
 
+    cout << "Your streak is now " << streak << " days(including rest days)." << endl;
 
     return 0;
 }
@@ -31,30 +34,30 @@ int main(int argc, char* argv[]){
 // Function Declararion
 bool didYouWorkout(string prompt){
     char response = ' ';
-    bool error = false;
 
     cout << prompt << endl;
     cin >> response;
 
+    while(cin.fail()){
+        cin.clear();
+        cin.ignore(256,'\n');
+        cout << "Invalid input. Plesse enter 'y'/'Y' or 'n'/'N'" << endl;
+        cin >> response;
+    }
+
     switch(response){
         case 'Y':
         case 'y':
-            cout << "";
+            return true;
             break;
 
         case 'N':
         case 'n':
+            return false;
             break;
 
         default:
-            cout << "Invalid input" << endl;
-            error = true;
             break;
     }
-
-    if(!error){
-
-    }
-
-    return true;
+    return false;
 }
